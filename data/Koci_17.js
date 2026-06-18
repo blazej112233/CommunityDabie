5111d00b40c8d87144c887020a69985e6803139d
blazej112233 <121027@student.upwr.edu.pl>
2026-06-18 21:54:53 +0200

    Add files via upload

---
+++ data/Koci_17.js
@@ -0,0 +1,1 @@
+var json_Koci_17 = {"type":"FeatureCollection","name":"Koci_17","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[17.085990675409491,51.102498799670187]}}]}
